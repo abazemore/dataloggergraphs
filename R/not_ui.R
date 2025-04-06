bff93b9f82a76ebf2905fa25e49242da9075596c
abazemore <avery.bazemore@gmail.com>
2025-04-06 20:28:59 +0100

    Deleted other logger files

--- R/not_ui.R
+++ R/not_ui.R
@@ -1,174 +1,138 @@
+# Load the packages and function list used in this script
 library(tidyverse)
-source("R/funs.R")
+source('R/funs.R')
 
 # Site information ----
 # Site name to use in tables and graph titles
-site <- "Lambeth Palace Library"
-# Abbreviation of site for filenames, use underscores rather than spaces
-site_short <- "LPL"
-# Currently supports "tinytag", "rotronic", and "trendbms"
-# Rotronic files must be converted to .csv because the .xls files are not readable
-brand <- "trendbms"
+site <- 'Anonymous Library'
 
-# Process the data and create summaries ----
+# Abbreviation of site for filenames, using underscores rather than spaces
+site_short <- 'AL'
+
+# Currently supports 'tinytag', 'rotronic', 'trendbms', 
+# 'tandd', 'miniclima', 'meaco', and 'previous' for csvs generated here.
+# Tinytag and T&D files must be converted to csv before use, but Rotronic xls files can be used directly.
+brand <- 'previous'
+
+# Process data ----
+# Copy the logfiles into dataloggergraphs-main/data, with folders for each brand.
 # Make list of filenames
-filenames <- dir("../sampledata/csv2021/tandd", full.names=TRUE)
-#Get data from files. This runs as a loop for each file.
+filenames <- dir('data/previous', full.names=TRUE)
+
+# Get data from files. This runs as a loop for each file.
 datalist <- lapply(filenames, parse_datalogger, site = site, brand = brand)
-envdata2 <- combine_data(datalist, brand)
-envdata <- full_join(envdata, envdata1)
 
-envdata <- filter(envdata2, temp < 25)
-# movetest <- graph_move(envdata1,envdata2,store1,store2,move_date,start_date,end_date)
-# movetest
-# Create tables with store max/min/mean and BS4971 compliance
-rotronic_data <- filter(envdata, grepl("Rotronic", model))
-tinytag_data <- bind_rows(filter(envdata, grepl("TGU", model)),
-                          filter(envdata, grepl("Tinytag", model)))
-tandd_data <- filter(envdata, grepl("T&D", model))
-trendbms_data <- filter(envdata, grepl("Trend", model))
-combined_envdata <- bind_rows(rotronic_data, tinytag_data, trendbms_data)
-site_summary <- summarise_site(envdata, start_date = "2020-09-01", end_date = "2021-09-01")
-rating_bs <- bs4971(envdata, start_date = "2020-09-01", end_date = "2021-09-01")
-comparison <- bind_rows(rotronic_summary, tinytag_summary, bms_summary)
-compare_subset <- filter(comparison, grepl("3A", location))
-dose <- light_dose(envdata1)
-# Write .csv files of all data and tables
-write_csv(envdata, str_c(date(min(envdata$datetime)),"_to_",
-                         date(max(envdata$datetime)), "_data_", 
-                         site_short, ".csv"))
-write_csv(site_summary, str_c(date(min(envdata$datetime)),"_to_",
-                         date(max(envdata$datetime)), "_summary_", 
-                         site_short, ".csv"))
-write_csv(rating_bs, str_c(date(min(envdata$datetime)),"_to_",
-                         date(max(envdata$datetime)), "_BS4971_", 
-                         site_short, ".csv"))
-graph_store(tandd_data, title = "Mezzanine display cases")
+# Combine the data from the list of files into one dataframe
+# If you have more than one brand of datalogger, use envdata_brand then combine in the next step.
+envdata <- combine_data(datalist)
 
-# Graph stores ----
+# Combine the envdata_brand files
+envdata <- bind_rows(envdata_tinytag, envdata_rotronic) %>%
+  distinct()
+
+# Clean data ----
+# Remove potentially faulty readings. The defaults are 5-35C and 10-80%.
+envadata_clean <- envdata
+envdata_clean <- remove_faulty(envdata_clean, max_temp = 30, min_temp = 8)
+
+# Subset the readings if you are compiling data for a specific period
+envdata_clean <- subset_readings(envdata_clean, start_date = '2024-01-01 00:00:00', end_date = '2024-12-31 23:59:59')
+
+# Some Tinytags store the model as 'Tinytag Ultra H�C/%RH', which can be trimmed
+envdata_clean$model <- str_remove(envdata_clean$model, ' H�C/%RH')
+
+# Correct locations if the brand has restrictions on names, like character limits or not allowing spaces
+envdata_clean$location <- str_replace_all(envdata_clean$location, "caseA", "case A")
+
+# If you would like to extract a specific area or set of areas, create a new dataframe
+envdata_exhib <- subset_readings(envdata_clean, store = 'Exhib')
+# Or exclude an area
+envdata_store <- subset_readings(envdata_clean, exclude_stores = 'Exhib')
+
+# If would like the summary to use different names, create a new dataframe
+envdata_store_rename <- envdata_store
+envdata_store_rename$location <- str_replace_all(envdata_store_rename$location, 'Arch ', 'Archive ')
+
+# If you have more than one logger in the space, numbered 'Archive 2B 1' and 'Archive 2B 2', remove the final number
+# ^ represents the start of the name and $ represents the end, so ' 1$' selects 'Archive 2A 1' but not 'Archive 1A'
+envdata_store_rename$location <- str_replace_all(envdata_store_rename$location, ' [1-3]$', '')
+
+# Summarise ----
+# Create tables with max/min/mean, mean and range, standard compliance, or light dose
+
+# Default 'monthly', accepts 'annual' and 'daily'
+# Includes the 1st and 99th percentile to trim unusual spikes
+site_summary <- summarise_site(envdata_clean, type = 'daily')
+# Default BS 4971, also accepts 'Icon', and 'PAS 198 25' or 'PAS 198 30' for 25 or 30C max
+# Or set own parameters
+comp <- compliance(envdata_clean, standard = 'Icon' #,
+                   #min_temp = 16, max_temp = 23, min_RH = 40, max_RH = 60
+)
+# Summary of light data with percentage of JNF for BW standards
+# Can be limited to exhibition period with start_date and end_date
+light <- light_dose(envdata_clean)
+
+# Write .csv files of all data and tables ----
+# Set standard and site and save
+envdata <- envdata_clean
+standard <- 'BS_4971'
+site_short <- 'GH'
+write_csv(envdata, paste0(date(min(envdata$datetime)),'_to_',
+                          date(max(envdata$datetime)), '_data_', 
+                          site_short, '.csv'))
+write_csv(monthly_summary, paste0(date(min(envdata$datetime)),'_to_',
+                                  date(max(envdata$datetime)), '_monthly_summary_', 
+                                  site_short, '.csv'))
+write_csv(annual_summary, paste0(date(min(envdata$datetime)),'_to_',
+                                 date(max(envdata$datetime)), '_annual_summary_', 
+                                 site_short, '.csv'))
+write_csv(comp, paste0(date(min(envdata$datetime)),'_to_',
+                       date(max(envdata$datetime)), '_', standard, '_compliance_', 
+                       site_short, '.csv'))
+write_csv(dose, paste0(date(min(dose$start_period)),'_to_',
+                       date(max(dose$end_period)), '_light_', 
+                       site_short, '.csv'))
+
+# Graph ----
 # Temperature and humidity graphs
-graph_store(envdata, start_date = "2020-09-01")
-# Change the value of "store" to graph a single store or group of stores sharing a pattern
-# For example "7" graphs "Archive 7", but "Arch" graphs all archive spaces
-graph_store(envdata, store = "3A", start_date = "2020-09-01")
-graph_summary(site_summary, start_date = "2020-09-01")
-# BS4971 compliance graphs, set of three for low/good/high temp and RH and overall
-bs_t <- graph_bs4971(rating_bs, "t")
-bs_r <- graph_bs4971(rating_bs, "R")
-bs_B <- graph_bs4971(rating_bs, "B")
-all_stores_graph
-store_graph
-bs_t
-bs_r
-bs_B
+# Graph all stores
+# If no title specified, 'All stores at [site]'
+# Accepts max_axis_temp and max_axis_RH, default 40 and 100
+graph_store(envdata_clean, min_temp = 16, max_temp = 20, min_RH = 40, max_RH = 60,
+            max_axis_RH = 80,
+            exclude_stores = 'D',
+            title = 'Anonymous Library main building')
 
-light_dose <-   envdata2 %>%
-  mutate(location = str_remove(location, " [1-3]$")) %>%
-  group_by(location, year = year(datetime), month = month(datetime)) %>%
-  summarise(max_lux = max(lux),
-            mean_lux = mean(lux))
-subset <- rotronic_data
-start_date <- "2021-05-01"
-end_date <- "2021-09-01"
-if(start_date != FALSE) {
-  subset <- filter(subset, datetime >= start_date)
-}
-if(end_date != FALSE) {
-  subset <- filter(subset, datetime <= end_date)
-}
+# Graph single store
+# Change the value of 'store' to graph a single store or group of stores sharing a pattern
+# For example '7' graphs 'Archive 7', but 'Arch' graphs all spaces named 'Archive'
+# Include a ^ at the beginning or $ at the end to match a certain part
+# 'E$' will match 'Exhibition Case E' but not 'Exhibition Case B'
+# The default title is the first location in the subset of the data, but can be changed
+graph_store(envdata_clean, min_temp = 16, max_temp = 20, min_RH = 40, max_RH = 60,
+            store = 'D',
+            title = 'Anonymous Library extension')
 
-if(start_date == FALSE) {
-  start_date <- min(subset$datetime)
-}
-if(end_date == FALSE) {
-  end_date <- max(subset$datetime)
-}
-# Graph single store ----
-# store is identifying part of location, does not have to match whole string
-store <- "3A"
-if(store != FALSE) {
-  message("Graphing single store")
-  subset_store <- filter(subset, grepl(store, location))
-  line_alpha <- 1
-  graph_title <- store
-  
-  graph_subtitle <- str_c(dmy_style(min(subset$datetime))," to ",
-                          dmy_style(max(subset$datetime)))
-}
-#Override title and subtitle if necessary
-#graph_title <- "" 
-#graph_subtitle <- ""
+# Graph summary
+graph_summary(envdata_clean)
 
-# Graph all stores ----
-if(store == FALSE) {
-  message("Graphing all stores")
-  if(excluded_stores != FALSE) {
-    subset <- filter(subset_store, !grepl(excluded_stores, location))
-  }
-  breaks <- "2 months"
-  min_breaks = "1 month"
-  line_alpha <- 0.5
-  graph_title <- str_c("All stores at ", subset$venue[1])
-  graph_subtitle <- str_c(dmy_style(min(subset$datetime))," to ",
-                          dmy_style(max(subset$datetime)))
-}
+# Save the last generated plot
+# Rerun this line with a different filename after each generated plot
+ggsave('AA all stores 2025.png', scale = 1.5, width = 150, height = 100, units = 'mm')
 
-# dmy_style <- stamp("31 December 2020", orders = "dmy")
-graph_subtitle <- str_c(dmy_style(min(subset$datetime))," to ",
-                        dmy_style(max(subset$datetime)))
-ggplot(subset_store, mapping = aes(x = datetime, group = location)) +
-  geom_hline(
-    yintercept = 23,
-    color = "red",
-    linetype = "dotted",
-    alpha = 0.8
-  ) +
-  geom_hline(
-    yintercept = 13,
-    color = "red",
-    linetype = "dotted",
-    alpha = 0.8
-  ) +
-  geom_hline(
-    yintercept = 60 / 2.5,
-    color = "blue",
-    linetype = "dotted",
-    alpha = 0.6
-  ) +
-  geom_hline(
-    yintercept = 35 / 2.5,
-    color = "blue",
-    linetype = "dotted",
-    alpha = 0.6
-  ) +
-  geom_line(aes(y = temp),
-            color = "red",
-            size = 0.25,
-            alpha = line_alpha) +
-  geom_line(aes(y = RH / 2.5),
-            color = "blue",
-            size = 0.25,
-            alpha = line_alpha) +
-  scale_x_datetime(name = "Date",
-                   # date_breaks = breaks,
-                   # minor_breaks = min_breaks,
-                   date_labels = "%m/%Y") +
-  labs(title = graph_title, subtitle = graph_subtitle) +
-  scale_y_continuous(
-    name = "Temperature (º C, red)",
-    limits = c(0, 40),
-    sec.axis = sec_axis( ~ . * 2.5,
-                         name = "Rel. Humidity (%, blue)")) +
-  theme(axis.title.y.left = element_text(color = "red"),
-        axis.title.y.right = element_text(color = "blue"))
+# Change the value of 'store' to graph a single store or group of stores sharing a pattern
+# For example '7' graphs 'Archive 7', but 'Arch' graphs all spaces named 'Archive'
+# Include a ^ at the beginning or $ at the end to match a certain part
+# 'E$' will match 'Exhibition Case E' but not 'Exhibition Case B'
+# The default title is the first location in the subset of the data, but can be changed
+graph_light(envdata_exhib, store = 'S', title = 'Exhibition SE & SW cases')
 
-cumulative <- group_by(envdata2, location, datetime) %>% 
-  summarise(location = location,
-            day = day()) %>%
-  mutate(cumlux = cumsum(lux))
-
-ggplot(envdata2, mapping = aes(x = datetime, group = location, color = location)) +
-  ylab("Cumulative lux hours") +
-  xlab("Date") +
-  geom_line(aes(y = cumsum(lux)))
+# Standard compliance graphs, set of three for low/good/high temp and RH and overall
+# Set o_t_r to o for overall, t for temperature, or r for RH
+# Include standard name or specify range
+# Set custom max/min or standards 'BS 4971', 'PAS 198 25' or 'PAS 198 30' for max 25 or 30C,
+#   'Icon' [2023 environmental guidance note], and 'Bizot'
+graph_compliance(envdata_clean, o_t_r = 'o',
+                 standard = 'Icon'
+)
 
